34fcf5a4167333b7c22b3e497ebf3e52a8d8db6b
Daniel Osineye <daniel.osineye@gmail.com>
2017-09-13 13:34:55 +0100

    Managing the DOM Event

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -2,14 +2,31 @@ import React from "react";
 import { Checklist } from "./Checklist";
 
 export class Card extends React.Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      showDetails: true
+    };
+  }
+  toggleDetails(){
+    this.setState({showDetails: !this.state.showDetails});
+  }
   render() {
-    return(
-      <div className="card">
-        <div className="card__title">{this.props.title}</div>
+    let cardDetails;
+    if(this.state.showDetails){
+      cardDetails = (
         <div className="class__details">
           {this.props.description}
           <Checklist cardId={this.props.id} tasks={this.props.tasks}/>
         </div>
+      );
+    };
+    return(
+      <div className="card">
+        <div className="card__title" onClick={this.toggleDetails.bind(this)}
+
+        >{this.props.title}</div>
+        {cardDetails}
       </div>
     );
   }
